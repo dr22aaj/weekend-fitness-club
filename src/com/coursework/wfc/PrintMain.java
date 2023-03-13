4b53809942f5a341bcb40c0350f040189788e6e3
dinushi ranasinghe <dr22aaj@herts.ac.uk>
2023-03-13 14:31:08 +0000

    Added main classes

--- src/com/coursework/wfc/PrintMain.java
+++ src/com/coursework/wfc/PrintMain.java
@@ -2,7 +2,7 @@ package com.coursework.wfc;
 
 public class PrintMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello new world!");
     }
 
 }
