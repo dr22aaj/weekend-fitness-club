4631549709012e8fae66d622ab60b6dcc4c7d150
dinushi ranasinghe <dr22aaj@herts.ac.uk>
2023-04-25 11:04:55 +0100

    adding test files

--- src/test/java/com/coursework/wfc/CommonTest.java
+++ src/test/java/com/coursework/wfc/CommonTest.java
@@ -8,8 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class CommonTest {
     Common myUnit = new Common();
     @Test
-    @DisplayName("Validate Attendance")
-    void isValidateAttendance() {
+    @DisplayName("Validate Attendance - True")
+    void isValidateAttendanceTrue() {
         String bookingNo = "BK021";
         boolean actual = myUnit.isValidateAttendance(bookingNo);
         assertTrue(actual);
@@ -28,5 +28,27 @@ class CommonTest {
         String result = "CUS002";
         String actual = myUnit.getCustomerNo(bookingNo);
         assertEquals(actual,result);
+    }
+    @Test
+    @DisplayName("Get Customer Number Fail")
+    void getCustomerNoFail() {
+        String bookingNo = "bk022";
+        String result = "CUS002";
+        String actual = myUnit.getCustomerNo(bookingNo);
+        assertNotEquals(actual,result);
+    }
+    @Test
+    @DisplayName("Validate File Path - False")
+    void validateFilePath() {
+        String path = "C:\\textF\\bookings.txt";
+        boolean actual = myUnit.validateFilePath(path);
+        assertFalse(actual);
+    }
+    @Test
+    @DisplayName("Validate File Path - True")
+    void validateFilePathTrue() {
+        String path = "C:\\textFiles\\bookings.txt";
+        boolean actual = myUnit.validateFilePath(path);
+        assertTrue(actual);
     }
 }
